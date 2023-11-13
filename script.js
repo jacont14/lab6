05769f8b3edc06686e10c199eec67944f81350c8
jacont14 <jacont14@asu.edu>
2023-11-13 08:59:40 -0700

    js update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your solution goes here 
+function isStrongPassword(password){
+    if(password.length < 8){
+        return false;
+    }
+    if(password.indexOf("password") !== -1){
+        return false;
+    }
+
+    let hasUppercase = false;
+    for(let i = 0; i < password.length; i++){
+        let charCode = password.charCodeAt(i);
+        if(charCode >= 65 && charCode <= 90){
+            hasUppercase = true;
+            break;
+        }
+    }
+
+    return hasUppercase;
+}
